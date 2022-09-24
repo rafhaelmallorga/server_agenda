42fd513febc2841911926b3031a5c132d84fd909
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-09-24 03:50:33 -0300

    feat :sparkles: created first path of swagger docs json

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,15 @@ import express from "express"
 import handleAppErrorMiddleware from "./middlewares/handleAppError.middleware";
 import appRoutes from "./routers";
 import "express-async-errors"
+import swaggerUi from "swagger-ui-express"
+import swaggerDocs from "./swagger.json"
+
 
 const app = express()
 
 app.use(express.json())
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 appRoutes(app)
 
