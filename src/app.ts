ba027be5f2141faa2713d315c811e7305a5e7bee
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-09-28 18:29:42 -0300

    fix :hammer: fixed ejs compilation

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import appRoutes from "./routers";
 import "express-async-errors"
 import swaggerUi from "swagger-ui-express"
 import swaggerDocs from "./swagger.json"
+import path from "path";
 
 
 const app = express()
@@ -11,6 +12,9 @@ const app = express()
 app.use(express.json())
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+app.set("views", path.join(__dirname, "report"))
+app.set("view engine", "ejs")
 
 appRoutes(app)
 
