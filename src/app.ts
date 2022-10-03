befce7f22dab1768a07685ce7115b6ec12e24b5f
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-10-03 09:38:03 -0300

    feat :sparkles: installed cors

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,13 @@ import "express-async-errors"
 import swaggerUi from "swagger-ui-express"
 import swaggerDocs from "./swagger.json"
 import path from "path";
+import cors from 'cors'
 
 
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.set("views", path.join(__dirname, "report"))
